6faac91a0d3ef665bbf54c8b2db5f5d4acca78fc
Leanherediadev <lea.200048@gmail.com>
2025-08-12 22:28:32 -0300

    Version 2: local host

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("¡Hello World!");
+// hello.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200; // Código HTTP 200 = OK
+  res.setHeader('Content-Type', 'text/plain'); // Tipo de contenido
+  res.end('Hello World\n'); // Mensaje que se envía al navegador
+});
+
+server.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
+
